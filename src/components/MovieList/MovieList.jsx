248c1850be3cea98ec836a14f1e73d69b97ed865
Nazar <nkinah93@gmail.com>
2025-03-24 18:45:02 +0200

    test-2

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,3 +1,15 @@
 import './MovieList.scss';
-
-export const MovieList = () => <>Put the list here</>;
+import { MovieCard } from '../MovieCard/MovieCard';
+import moviesFromServer from '../../api/movies.json';
+export const MovieList = () =>  {
+  return (
+    <div className="movies" data-cy="MoviesList">
+      {moviesFromServer.map(movie => (
+        <MovieCard
+          key={movie.imdbId}
+          movie={movie}
+        />
+      ))}
+    </div>
+  )
+};
